81c95411967c5b9f75e937da08644a58a7b04612
Ethan Bennis <ethan.bennis@gmail.com>
2021-03-07 22:48:07 -0800

    Minor README cleanup. Describe hit detection example.

--- README.md
+++ README.md
@@ -16,10 +16,10 @@ applications involving 3D math.
 
 ### Goals
 Some of my personal goals for this project are:
-* **Learn about 3D math fundamentals**. This is a 3D math library, after all!
+* **Learn about 3D math fundamentals**. Get more comfortable with vectors, matrices,
+  transformations, homogeneous coordinates, quaternions, etc. in a programming context.
 * **Explore various features of modern C++**. I've been restricted to an old C++11 toolchain at 
-  my job, so I want to learn about some of the newer features of C++14/17/20 via a personal
-  project.
+  my job, so I want to learn about some of the newer features of C++14/17/20 via this project.
 * **Utilize the standard library where possible**. I was a C programmer long before I started 
   writing C++, and one area of C++ I've been slow to adopt is the standard library's numerous
   containers and algorithms.
@@ -27,7 +27,7 @@ Some of my personal goals for this project are:
   `constexpr`, and C++20 provides `constexpr` standard algorithms. Surprisingly, the last missing 
   piece is basic math operations like `sqrt()`. I'm providing `constexpr` implementations of these 
   operations, but they're only used at compile-time thanks to `std::is_constant_evaluated()`.
-* **Follow through**: Complete a fully-functional library, including good test coverage and 
+* **Follow through**. Complete a fully-functional library, including good test coverage and
   documentation.
 * **Use this library to build something else**. Details to be determined, but this should serve as 
   a base for future work involving graphics, games, and/or robotics programming.
@@ -35,19 +35,19 @@ Some of my personal goals for this project are:
 ### Requirements / Dependencies
 
 Vec is built using modern C++ features up to and including several provided by C++20. As of this 
-time, this library has only been tested with **GCC version 10**, though recent versions of Clang
-and MSVC are hopefully compatible.
+time, this library has only been tested with **GCC version 10**, though comparably recent 
+versions of Clang and MSVC are hopefully compatible.
 
-Unit tests for vec use [doctest](https://github.com/onqtam/doctest), which is included as a 
+Unit tests for Vec use [doctest](https://github.com/onqtam/doctest), which is included as a 
 submodule in this repository. If you want to run unit tests locally, be sure to update submodules 
 after cloning this repo:
 ```bash
 git submodule update --init
 ```
 
-### API
-*TODO - Need to decide whether to do this manually or with something like doxygen...*
-
 ### Examples
 See the included [target hit detection example](examples/target_hit_detection.cpp) for a
 demonstration of basic vector arithmetic. Additional examples will be added in the future.
+
+### API
+*TODO - Need to decide whether to do this manually or with something like doxygen...*
--- examples/target_hit_detection.cpp
+++ examples/target_hit_detection.cpp
@@ -1,4 +1,65 @@
 // Vec library usage example: target hit detection
+//
+// This is an example of a basic interview-style problem on vector geometry for game programming.
+// The premise is that we want to implement the hit-detection logic for a simple shooting gallery
+// mini-game. There exists a flat, circular target at some position and orientation in 3D space, and
+// a player shooting in a known direction from some other point in space.
+//
+// We can fully describe a circular target with the following:
+//     1. The point in space at which the center of the target lies (p0)
+//     2. The vector normal to the plane on which the target lies (n)
+//     3. The radius of the target (r)
+//
+// Similarly, we can fully describe a player shot with the following:
+//     1. The point in space at which the player is shooting from (s)
+//     2. The vector describing the direction of the player's shot (v)
+//
+// The player's shot projects a ray with parametric equation:
+//
+//     ps = s + tv
+//
+// Where t is a scalar in the range [0, inf) since we only want to project in the forward direction.
+// We can sweep t across this range to generate any point ps along the trajectory of the shot.
+//
+// The target lies on a plane described by the implicit equation:
+//
+//     (p - p0) dot n = 0
+//
+// The vector formed by the difference between any arbitrary point p on the plane and the known
+// center of the target p0 will be orthogonal to the normal vector, meaning the dot product is zero.
+//
+// We can plug the above parametric ray equation in for point p in the implicit plane equation
+// and attempt to solve for t to see if the ray actually intersects the plane:
+//
+//     (s + tv - p0) dot n = 0
+//     ((s - p0) dot n) + (tv dot n) = 0
+//     tv dot n = (p0 - s) dot n
+//     t = ((p0 - s) dot n) / (v dot n)
+//
+// At this point we can check the denominator (v dot n) for zero, as that would blow up the
+// equation and also indicates that the shot is parallel to the surface of the plane, meaning
+// there is no intersect point (unless the shooting point s was actually on the surface of the
+// plane, which we'll choose to not worry about).
+//
+// Assuming the denominator is not zero, we can solve for t. If t is positive, there is an
+// intersect point in the direction of the player's shot. If t is negative, then there is an
+// intersect on the line, but in the opposite direction of the player's shot, so the shot is
+// obviously a miss.
+//
+// If t is positive, we can plug that back into the original ray equation to solve for the actual
+// intersect point:
+//
+//     p1 = s + tv
+//
+// We can then check the euclidean distance between intersect point p1 and target center point
+// p0, and compare that to the radius of the target. However, calculating the euclidean distance
+// requires taking the square root of (p1 - p0) dot (p1 - p0), and square roots are relatively
+// expensive. It's more computationally efficient to instead square the radius and compare the
+// square of both sides:
+//
+//     If (p1 - p0) dot (p1 - p0) < r^2 , the shot is a hit.
+//
+// This logic is implemented below in check_target_hit().
 
 #include <iostream>
 #include <iomanip>
@@ -11,13 +72,13 @@ using std::endl;
 
 // Shot parameters
 struct Shot {
-    Vec3f position;  // Position of shot relative to arbitrary origin [m,m,m]
+    Vec3f position;  // Position of shot relative to arbitrary origin [m_x,m_y,m_z]
     Vec3f direction; // Direction of shot [unitless]
 };
 
 // Target parameters
 struct CircleTarget {
-    Vec3f position; // Position of center of target relative to arbitrary origin [m,m,m]
+    Vec3f position; // Position of center of target relative to arbitrary origin [m_x,m_y,m_z]
     Vec3f normal;   // Normal of the plane on which the target lies [unitless]
     float radius;   // Radius of target [m]
 };
